7c3ac0ba63bde1ba99cf413c8ad87b696c83f858
Mehmetkurt676767 <vodafonepay496@gmail.com>
2024-10-29 19:31:57 +0300

    Update owner.py

--- plugins/extra/owner.py
+++ plugins/extra/owner.py
@@ -1,35 +1,45 @@
+# Power By @BikashHalder & @AdityaHalder 
+# Join @BikashGadgetsTech For More Update
+# Join @AdityaCheats For Hack
+# Join Our Chats @Bgt_Chat & @Adityadiscus 
+
 from pyrogram import Client, filters
 
 from Bikash import app
-from Bikash.utils.bgtmusic.bk import command
 
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 
 @app.on_message(
-    filters.command("owner")
-    )
-async def owner(client: Client, message: Message):
+    filters.command("sahibi")
+    & filters.group)
+async def repo(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"https://te.legra.ph/file/f73af9a4ffe130a83d8d2.jpg",
-        caption=f"""🥀 𝐂𝐥𝐢𝐜𝐤 𝐇𝐞𝐫𝐞 𝐅𝐨𝐫 𝐂𝐨𝐧𝐭𝐚𝐜𝐭 𝐎𝐰𝐧𝐞𝐫𝐬 𝐁𝐢𝐤𝐚𝐬𝐡 𝐎𝐫 𝐊𝐚𝐚𝐥, 𝐈𝐟 𝐘𝐨𝐮 𝐖𝐚𝐧𝐭 𝐏𝐫𝐨𝐦𝐨𝐭𝐞 𝐘𝐨𝐮𝐫 𝐆𝐫𝐨𝐮𝐩𝐬 𝐎𝐫 𝐎𝐭𝐡𝐞𝐫𝐬 𝐋𝐢𝐧𝐤 𝐓𝐡𝐞𝐧 [𝐂𝐥𝐢𝐜𝐤 𝐇𝐞𝐫𝐞](https://t.me/BgtPromote) & 𝐂𝐥𝐢𝐜𝐤 𝐎𝐭𝐡𝐞𝐫'𝐬 𝐁𝐮𝐭𝐭𝐨𝐧 & 𝐉𝐨𝐢𝐧 𝐎𝐮𝐫 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 𝐎𝐫 𝐆𝐫𝐨𝐮𝐩.. 🥀 [𝐘𝐨𝐮𝐭𝐮𝐛𝐞](https://youtube.com/@bikashgadgetstech).""",
+        photo=f"https://telegra.ph/Help-08-24-13",
+        caption=f"""🥀 bize destek olduğun için teşekkür ederiz:
+  [KAYNAK KOD ](https://github.com/kumsalfed5456/kumsalXmusic)""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "🥀 𝐁𝐢𝐤𝐚𝐬𝐡 🥀", url=f"https://t.me/BikashHalder")
+                        "🥀 admin 🥀", url=f"https://t.me/sevimsiz_biri")
             ],          
             [
                     InlineKeyboardButton(
-                        "🥀 𝐊𝐚𝐚𝐥 🥀", url=f"https://t.me/AdityaHalder")
+                        "🥀 destek 🥀", url=f"https://t.me/kumsaldestekkanal")
                 ],
                 [
                     InlineKeyboardButton(
-                        "🥀 𝐒𝐮𝐩𝐩𝐨𝐫𝐭 🥀", url=f"https://t.me/Bgt_Chat"
+                        "🥀 sohbet 🥀", url=f"https://t.me/GeceSohbettr"
                     ),
                     InlineKeyboardButton(
-                        "🥀 𝐔𝐩𝐝𝐚𝐭𝐞𝐬 🥀", url=f"https://t.me/BikashGadgetsTech")
+                        "🥀 𝐔𝐩𝐝𝐚𝐭𝐞𝐬 🥀", url=f"https://t.me/Ragnarben")
                 ]
             ]
         ),
-    )
+    ) 
+
+# Power By @BikashHalder & @AdityaHalder 
+# Join @BikashGadgetsTech For More Update
+# Join @AdityaCheats For Hack
+# Join Our Chats @Bgt_Chat & @Adityadiscus 
