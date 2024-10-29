7d1c703e821e273dbad4c72e11c2cc54e65a2be2
Mehmetkurt676767 <vodafonepay496@gmail.com>
2024-10-29 21:28:40 +0300

    Update song.py

--- Bikash/utils/inline/song.py
+++ Bikash/utils/inline/song.py
@@ -15,7 +15,7 @@ def song_markup(_, vidid):
         ],
         [           
             InlineKeyboardButton(
-                text="📱 YouTube 📱", url=f"https://youtube.com/@BikashGadgetsTech"
+                text="📱 admin 📱", url=f"https://t.me/sevimsiz_biri"
             ),
         ],
         [
